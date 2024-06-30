a02f18862c6e2add46cef16f123f50c315902116
Illia Katerynych <iliakat.kr@gmail.com>
2024-06-30 16:20:16 +0100

    mageMasterArmor added

--- src/main/java/com/velocitypowered/arcane/collections/Armor.java
+++ src/main/java/com/velocitypowered/arcane/collections/Armor.java
@@ -1,6 +1,8 @@
 package com.velocitypowered.arcane.collections;
 
 import com.velocitypowered.arcane.armor.mageApprenticeArmor;
+import com.velocitypowered.arcane.armor.mageMasterArmor;
+
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -29,6 +31,17 @@ public enum Armor {
                 mageApprenticeArmor.bootsCraftRecipe(plugin);
                 break;
             case MAGE_MASTER:
+                mageMasterArmor mageMasterArmor = new mageMasterArmor();
+
+                helmet = mageMasterArmor.mageMasterHelmet();
+                chestplate = mageMasterArmor.mageMasterChestplate();
+                leggings = mageMasterArmor.mageMasterLeggings();
+                boots = mageMasterArmor.mageMasterBoots();
+
+                mageMasterArmor.helmetCraftRecipe(plugin);
+                mageMasterArmor.chestplateCraftRecipe(plugin);
+                mageMasterArmor.leggingsCraftRecipe(plugin);
+                mageMasterArmor.bootsCraftRecipe(plugin);
                 break;
         }
     }
